d5fb86e8a4a0ef137429a106cc379481dbc89bc4
Javier Jara Renteria <javierjararenteria@gmail.com>
2025-06-11 01:16:55 -0600

    maquetado basico

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   site: "https://Demurest33.github.io",
   base: "correconhistoria",
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
