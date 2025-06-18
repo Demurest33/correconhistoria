b3a7e5ef62e137a5b4ee5b995d5d2de80e873876
Javier Jara Renteria <javierjararenteria@gmail.com>
2025-06-18 09:46:36 -0600

    quizas sin base en los links

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://Demurest33.github.io",
-  base: "/correconhistoria",
+  base: "/",
 
   vite: {
     plugins: [tailwindcss()],
